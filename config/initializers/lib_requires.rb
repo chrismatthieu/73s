45e71f4d15dc9efb460785d1d9365824c846ff63
Chris Matthieu <chris@matthieu.us>
2011-05-18 08:05:49 -0700

    updated gems

--- config/initializers/lib_requires.rb
+++ config/initializers/lib_requires.rb
@@ -4,7 +4,7 @@ require 'ostruct'
 require 'william_paginate'
 require 'file_column'
 # require 'acts_as_ferret'
-require 'youtube_g'
+# require 'youtube_g'
 require 'colored'
 
 Less::JsRoutes.generate!
